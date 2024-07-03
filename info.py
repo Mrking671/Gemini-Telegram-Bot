5afc23da21f1689a6c10c211cdcf0eb3c1356cca
Mrking671 <telegrambot969797@gmail.com>
2024-07-03 06:19:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
 
 from os import environ
 
-API = environ.get("API", "") # shortlink api
-URL = environ.get("URL", "") # shortlink domain without https://
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+API = environ.get("API", "7d706f6d7c95ff3fae2f2f40cff10abdc0e012e9") # shortlink api
+URL = environ.get("URL", "linkshortify.com") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/ezpzsupport/17") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "Aibardai_bot") # bot username without @
 VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
